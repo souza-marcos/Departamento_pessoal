ef232a66612c2096fd446a1536b053ec8432cbc3
Marcos Daniel <marcosnumber11@gmail.com>
2021-10-28 11:23:07 -0300

    Headers fixed

--- src/arraylist.hpp
+++ src/arraylist.hpp
@@ -1,1 +1,26 @@
+#ifndef ARRAYLIST_HPP
+#define ARRAYLIST_HPP
+
+template <typename TData>
+struct ArrayList;
+
+template <typename TData>
+ArrayList<TData> *create(int maxSize);
+
+template <typename TData>
+bool isEmpty(ArrayList<TData> *list);
+
+template <typename TData>
+bool insertItem(ArrayList<TData> *list, TData item, int tamMax);
+
+template <typename TData>
+void printList(ArrayList<TData> *list, void (*print)(TData));
+
+template <typename TData>
+int searchItem(ArrayList<TData> *list, int val, bool (*compare)(TData, int));
+
+template <typename TData>
+bool deleteItem(ArrayList<TData> *list, int val, bool (*compare)(TData, int));
+
 #include "arraylist.cpp"
+#endif
--- src/linkedlist.hpp
+++ src/linkedlist.hpp
@@ -1,1 +1,29 @@
+#ifndef LINKEDLIST_HPP
+#define LINKEDLIST_HPP
+
+template <typename TData>
+struct Node;
+
+template <typename TData>
+struct LinkedList;
+
+template <typename TData>
+LinkedList<TData> *create();
+
+template <typename TData>
+void insertItem(TData e, LinkedList<TData> *list);
+
+template <typename TData>
+bool isEmpty(LinkedList<TData> *list);
+
+template <typename TData>
+Node<TData> *searchItem(LinkedList<TData> *list, int val, bool (*compare)(TData, int));
+
+template <typename TData>
+void printList(LinkedList<TData> *list, void (*print)(TData));
+
+template <typename TData>
+bool deleteItem(LinkedList<TData> *list, Node<TData> *&node);
+
 #include "linkedlist.cpp"
+#endif
